bf479021b517c0539a7b6592c059190eb0e157be
Arthur Rolemberg <arthurrolemberg4262@gmail.com>
2024-02-23 17:21:10 -0300

    Creates content pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,41 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'titulo',
+    description: 'tudo de bom',
+    photoCover: 'link',
+  },
+  {
+    id: '2',
+    title: 'NOVO JOGO ANUNCIADO NA GAME FESTIVAL',
+    description: 'tudo de bom',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.wfHrO4vQwgt82WCMlhG99gHaEK?rs=1&pid=ImgDetMain',
+  },
+  {
+    id: '3',
+    title: 'NOVAS SKINS DO SUPERCELL MAKER CHEGARÃO NO BRAWL STARS',
+    description: 'tudo de bom',
+    photoCover:
+      'https://th.bing.com/th/id/OIF.fGCzRWoBtBsmOMW5eZkYmQ?rs=1&pid=ImgDetMain',
+  },
+  {
+    id: '4',
+    title: 'ANIME DE SOLO LEVELING ESTÁ SENDO LANÇADO NA CRUNCHYROLL',
+    description: 'tudo de bom',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.I7R9N2LIoOXPIm0gDcf86gHaEK?rs=1&pid=ImgDetMain',
+  },
+  {
+    id: '5',
+    title: 'titulo',
+    description: 'tudo de bom',
+    photoCover: 'link',
+  },
+  {
+    id: '6',
+    title: 'titulo',
+    description: 'tudo de bom',
+    photoCover: 'link',
+  },
+];
